57e0de9b9e9fa2197857f1e8e4b90933421ed357
nizamicup <nizam.icup@gmail.com>
2017-01-16 16:08:37 +0100

    final commit

---
+++ build/generated-sources/ap-source-output/h4/currency/model/Currency_.java
@@ -0,0 +1,14 @@
+package h4.currency.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-15T22:08:37")
+@StaticMetamodel(Currency.class)
+public class Currency_ { 
+
+    public static volatile SingularAttribute<Currency, Float> rate;
+    public static volatile SingularAttribute<Currency, String> currency;
+
+}
--- src/java/h4/currency/view/CurrencyBean.java
+++ src/java/h4/currency/view/CurrencyBean.java
@@ -6,6 +6,7 @@
 package h4.currency.view;
 
 import h4.currency.controller.CurrencyFacade;
+import h4.currency.model.CurrencyDTO;
 import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.enterprise.context.Conversation;
@@ -21,19 +22,63 @@ import javax.inject.Inject;
  */
 @Named(value = "currencyBean")
 @ConversationScoped
-public class CurrencyBean implements Serializable{
+public class CurrencyBean implements Serializable {
 
     @EJB
     CurrencyFacade currencyFacade;
     private String homeCurrency;
-     private String forignCurrency;
-    private float rate;
+    private String forignCurrency;
+    private float rate=0.0f;
+   private Float Result=0.0f; 
+    private Float money=0.0f;
+    
+    private CurrencyDTO homeCurrencyRate;
+    private CurrencyDTO forignCurrencyRate;
+    private Exception transactionFailure;
     @Inject
     private Conversation conversation;
+
+    private void startConversation() {
+        if (conversation.isTransient()) {
+            conversation.begin();
+        }
+    }
+
+    private void stopConversation() {
+        if (!conversation.isTransient()) {
+            conversation.end();
+        }
+    }
+
+    private void handleException(Exception e) {
+        stopConversation();
+        e.printStackTrace(System.err);
+        transactionFailure = e;
+    }
+    
+    public boolean getSuccess() {
+        return transactionFailure == null;
+    }
+
+    /**
+     * Returns the latest thrown exception.
+     */
+    public Exception getException() {
+        return transactionFailure;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+   
     /**
      * Creates a new instance of CurrencyBean
      */
-    
+
     public CurrencyBean() {
     }
 
@@ -60,5 +105,42 @@ public class CurrencyBean implements Serializable{
     public void setRate(float rate) {
         this.rate = rate;
     }
+    
+    public Float getResult() {
+        return Result;
+    }
+
+    public void Convert() {
+        
+         startConversation();
+         transactionFailure = null;
+
+        
+        homeCurrencyRate = currencyFacade.findCurrency(homeCurrency);
+        forignCurrencyRate = currencyFacade.findCurrency(forignCurrency);
+        if(homeCurrency.equals("USD"))
+        {
+           rate = homeCurrencyRate.getRate()*forignCurrencyRate.getRate();
+        }
+        if(homeCurrency.equals(forignCurrency))
+        {
+            rate = homeCurrencyRate.getRate()/forignCurrencyRate.getRate();
+        }
+        
+      if(money<0)
+        {
+                     handleException(new Exception("The money must be a positive float number."));
+        }
+        
+        else
+        {
+            rate = forignCurrencyRate.getRate()/homeCurrencyRate.getRate();
+             Result=rate*money;
+        }
+          
+    }
+    
    
+    
+
 }
